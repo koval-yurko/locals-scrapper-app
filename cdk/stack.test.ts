48342571922285ae47f7556d357d1418c9cc8f44
Yurii Kovalchuk <failwin@ukr.net>
2025-12-19 09:52:33 +0200

    add CDK fine-grained assertions tests

--- cdk/stack.test.ts
+++ cdk/stack.test.ts
@@ -4,7 +4,7 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { LocalsScrapperAppInfraStack } from './stack';
 
 describe('LocalsScrapperAppInfraStack Tests', () => {
-  test('SQS Queue Created', () => {
+  test('template is correct', () => {
     const app = new cdk.App();
     const stack = new LocalsScrapperAppInfraStack(app, 'MyTestStack', {
       env: {
@@ -17,6 +17,25 @@ describe('LocalsScrapperAppInfraStack Tests', () => {
         'arn:aws:acm:us-east-1:000000000000:certificate/00000000-xxxx-xxxx-xxxx-000000000000',
     });
     const template = Template.fromStack(stack);
+
     expect(template.toJSON()).toMatchSnapshot();
+  });
+
+  test('contains primary resources', () => {
+    const app = new cdk.App();
+    const stack = new LocalsScrapperAppInfraStack(app, 'MyTestStack', {
+      env: {
+        account: '000000000000',
+        region: 'us-east-1',
+      },
+      stage: 'dev',
+      domainName: 'DOMAIN_NAME',
+      certificateArn:
+        'arn:aws:acm:us-east-1:000000000000:certificate/00000000-xxxx-xxxx-xxxx-000000000000',
+    });
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::S3::Bucket', 1);
+    template.resourceCountIs('AWS::CloudFront::Distribution', 1);
   });
 });
